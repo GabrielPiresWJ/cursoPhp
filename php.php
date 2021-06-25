b79bd42870af08a0b451c467ad6d366efa88b05e
Gabriel P <gabriel.oliveira@webjump.com.br>
2021-06-25 12:43:00 -0300

    primeira commit

---
+++ php.php
@@ -0,0 +1,8 @@
+<?php
+
+
+echo "oi ";
+$idade = 21 ;
+$idadeFuturo = $idade ** 2;
+echo $idadeFuturo;
+echo gettype($idadeFuturo);
---
+++ string.php
@@ -0,0 +1,11 @@
+<?php
+$idade = 21;
+
+echo 'Olá Mundo, minha idade é ' . $idade . ' anos' . "\n";
+echo "Olá mundo, minha idade é $idade anos \n";
+echo "\t a \n";
+
+
+echo 'Olá Mundo, minha idade é ' . $idade . ' anos' . PHP_EOL;
+echo "Olá mundo, minha idade é $idade anos" . PHP_EOL;
+echo "\t a \n";
